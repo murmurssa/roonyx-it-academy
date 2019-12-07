8de2afbc1ddaab7f54d9ef30cc02a33617450819
Alisa Naidyonova <murmurssa@yandex.ru>
2019-12-07 12:18:53 +0300

    fix solution IT-134

--- PW-3/webpack-starter/src/app/JS/03-chess-board.js
+++ PW-3/webpack-starter/src/app/JS/03-chess-board.js
@@ -1,15 +1,12 @@
 /* eslint-disable prefer-const */
-let size = 8;
-let board = '';
-
-for (let y = 0; y < size; y += 1) {
-  for (let x = 0; x < size; x += 1) {
-    if ((x + y) % 2 === 0) {
-      board += ' ';
-    } else {
-      board += '#';
+function chessBoard(size) {
+  let board = '';
+  for (let y = 0; y < size; y += 1) {
+    for (let x = 0; x < size; x += 1) {
+      board += (x + y) % 2 ? '#' : ' ';
     }
+    board += '\n';
   }
-  board += '\n';
+  console.log(board);
 }
-console.log(board);
+chessBoard(8);
