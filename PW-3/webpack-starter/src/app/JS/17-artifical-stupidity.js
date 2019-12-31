199425de3a73a46ba7dd8619a7982c27ba0d633a
murmurssa <alissamurmurss@gmail.com>
2019-12-31 14:21:25 +0300

    separate code IT-148

--- PW-3/webpack-starter/src/app/JS/17-artifical-stupidity.js
+++ PW-3/webpack-starter/src/app/JS/17-artifical-stupidity.js
@@ -1,57 +1,4 @@
 /* eslint-disable */
-(function () {
-  'use strict';
-
-  let active = null;
-
-  function Animated(world) {
-    this.world = world;
-    let outer = (window.__sandbox ? window.__sandbox.output.div : document.body), doc = outer.ownerDocument;
-    let node = outer.appendChild(doc.createElement('div'));
-    node.style.cssText = 'position: relative; width: intrinsic; width: fit-content;';
-    this.pre = node.appendChild(doc.createElement('pre'));
-    this.pre.appendChild(doc.createTextNode(world.toString()));
-    this.button = node.appendChild(doc.createElement('div'));
-    this.button.style.cssText = 'position: absolute; bottom: 8px; right: -4.5em; color: white; font-family: tahoma, arial; ' +
-      'background: #4ab; cursor: pointer; border-radius: 18px; font-size: 70%; width: 3.5em; text-align: center;';
-    this.button.innerHTML = 'stop';
-    let self = this;
-    this.button.addEventListener('click', function () { self.clicked(); });
-    this.disabled = false;
-    if (active) active.disable();
-    active = this;
-    this.interval = setInterval(function () { self.tick(); }, 333);
-  }
-
-  Animated.prototype.clicked = function () {
-    if (this.disabled) return;
-    if (this.interval) {
-      clearInterval(this.interval);
-      this.interval = null;
-      this.button.innerHTML = 'start';
-    } else {
-      let self = this;
-      this.interval = setInterval(function () { self.tick(); }, 333);
-      this.button.innerHTML = 'stop';
-    }
-  };
-
-  Animated.prototype.tick = function () {
-    this.world.turn();
-    this.pre.removeChild(this.pre.firstChild);
-    this.pre.appendChild(this.pre.ownerDocument.createTextNode(this.world.toString()));
-  };
-
-  Animated.prototype.disable = function () {
-    this.disabled = true;
-    clearInterval(this.interval);
-    this.button.innerHTML = 'Disabled';
-    this.button.style.color = 'red';
-  };
-
-  window.animateWorld = function (world) { new Animated(world); };
-})();
-
 let plan = ['############################',
   '#      #    #      o      ##',
   '#                          #',
@@ -332,10 +279,10 @@ function SmartPlantEater() {
   this.direction = 'e';
 }
 SmartPlantEater.prototype.act = function (view) {
-  var space = view.find(' ');
+  let space = view.find(' ');
   if (this.energy > 90 && space)
     return { type: 'reproduce', direction: space };
-  var plants = view.findAll('*');
+  let plants = view.findAll('*');
   if (plants.length > 1)
     return { type: 'eat', direction: randomElement(plants) };
   if (view.look(this.direction) != ' ' && space)
@@ -343,23 +290,23 @@ SmartPlantEater.prototype.act = function (view) {
   return { type: 'move', direction: this.direction };
 };
 
-animateWorld(new LifelikeWorld(
+let valley = new LifelikeWorld(
   ['############################',
-    '#####              X  ######',
-    '##   ***    X           **##',
+    '#####              Ж  ######',
+    '##   ***    Ж           **##',
     '#   *##**         **  O  *##',
     '#    ***     O    ##**    *#',
     '#       O         ##***    #',
     '#                 ##**     #',
     '#   O       #*             #',
     '#*          #**       O    #',
-    '#***    X   ##**    O    **#',
+    '#***    Ж   ##**    O    **#',
     '##****     ###***       *###',
     '############################'],
   {
     '#': Wall,
-    'X': SmartPlantEater,
+    'Ж': SmartPlantEater,
     'O': PlantEater,
     '*': Plant
   }
-));
+);
