e57667c3175f2a5d4aa1726f083ceeb56a9204cc
murmurssa <alissamurmurss@gmail.com>
2020-01-13 19:27:49 +0300

    fix solution IT-146

--- PW-3/webpack-starter/src/app/JS/15-another-cell.js
+++ PW-3/webpack-starter/src/app/JS/15-another-cell.js
@@ -1,5 +1,4 @@
-/* eslint-disable func-names */
-// основа из учебника
+/* eslint-disable max-classes-per-file */
 function repeat(string, times) {
   let result = '';
   for (let i = 0; i < times; i += 1) {
@@ -8,44 +7,48 @@ function repeat(string, times) {
   return result;
 }
 
-function TextCell(text) {
-  this.text = text.split('\n');
+class TextCell {
+  constructor(text) {
+    this.text = text.split('\n');
+  }
+
+  minWidth() {
+    return this.text.reduce((width, line) => Math.max(width, line.length), 0);
+  }
+
+  minHeight() {
+    return this.text.length;
+  }
+
+  draw(width, height) {
+    const result = [];
+    for (let i = 0; i < height; i += 1) {
+      const line = this.text[i] || '';
+      result.push(repeat(' ', width - line.length) + line);
+    }
+    return result;
+  }
 }
 
-TextCell.prototype.minWidth = function () {
-  return this.text.reduce((width, line) => Math.max(width, line.length), 0);
-};
-
-TextCell.prototype.minHeight = function () {
-  return this.text.length;
-};
-
-TextCell.prototype.draw = function (width, height) {
-  const result = [];
-  for (let i = 0; i < height; i += 1) {
-    const line = this.text[i] || '';
-    result.push(repeat(' ', width - line.length) + line);
+class StretchCell {
+  constructor(inner, width, height) {
+    this.inner = inner;
+    this.width = width;
+    this.height = height;
   }
-  return result;
-};
-// моё решение
-const StretchCell = function (inner, width, height) {
-  this.inner = inner;
-  this.width = width;
-  this.height = height;
-};
 
-StretchCell.prototype.minWidth = function () {
-  return Math.max(this.inner.minWidth(), this.width);
-};
+  minWidth() {
+    return Math.max(this.inner.minWidth(), this.width);
+  }
 
-StretchCell.prototype.minHeight = function () {
-  return Math.max(this.inner.minHeight(), this.height);
-};
+  minHeight() {
+    return Math.max(this.inner.minHeight(), this.height);
+  }
 
-StretchCell.prototype.draw = function (width, height) {
-  return this.inner.draw(width, height);
-};
+  draw(width, height) {
+    return this.inner.draw(width, height);
+  }
+}
 
 const sc = new StretchCell(new TextCell('abc'), 1, 2);
 console.log(sc.minWidth());
