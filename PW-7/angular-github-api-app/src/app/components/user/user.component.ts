e43897011912fc5f74b55e45569f6f23dd223bd2
murmurssa <alissamurmurss@gmail.com>
2020-01-29 14:14:14 +0300

    add styles for user component HW-3

--- PW-7/angular-github-api-app/src/app/components/user/user.component.ts
+++ PW-7/angular-github-api-app/src/app/components/user/user.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { User } from '../../interfaces';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class UserComponent{
+  @Input()
+  users: User[];
 }
